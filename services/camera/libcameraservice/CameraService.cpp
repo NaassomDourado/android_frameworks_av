b085b2089924d5226739a85bca4722c3353daf8e
NaassomDourado <naassomdourado@gmail.com>
2022-01-20 16:14:26 -0300

    Revert "libcameraservice: add TARGET_CAMERA_NEEDS_CLIENT_INFO"
    This reverts commit ffb937a863eadcdb7caf1876947a47b5fdca3531.

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -25,10 +25,6 @@
 #include <cstring>
 #include <ctime>
 #include <string>
-#ifdef CAMERA_NEEDS_CLIENT_INFO
-#include <iostream>
-#include <fstream>
-#endif
 #include <sys/types.h>
 #include <inttypes.h>
 #include <pthread.h>
@@ -1590,6 +1586,7 @@ Status CameraService::connect(
                 ret.toString8());
         return ret;
     }
+
     *device = client;
     return ret;
 }
@@ -3344,11 +3341,6 @@ status_t CameraService::BasicClient::finishCameraStreamingOps() {
                 mClientPackageName, mClientFeatureId);
         mOpsStreaming = false;
     }
-#ifdef CAMERA_NEEDS_CLIENT_INFO
-    std::ofstream cpf("/data/misc/lineage/client_package_name");
-    std::string cpn = String8(mClientPackageName).string();
-    cpf << cpn;
-#endif
     return OK;
 }
 
